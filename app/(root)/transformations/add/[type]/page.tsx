ee4f12122681eeff9b215c63ccdd0d66c3a4c25d
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-06-26 14:24:04 +0200

    created a dynamic header for transformations & transformation form component

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,16 @@
+import Header from "@/components/shared/header";
+import TransformationForm from "@/components/shared/transformation-form";
+import { transformationTypes } from "@/constants";
 import React from "react";
 
-const AddTransformationTypePage = () => {
-  return <div>AddTransformationTypePage</div>;
+const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+  const transformation = transformationTypes[type];
+  return (
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+      <TransformationForm />
+    </>
+  );
 };
 
 export default AddTransformationTypePage;
